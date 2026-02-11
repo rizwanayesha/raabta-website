3b34feebe928f3d1fcd076e4c29893d5e2c8ed89
rizwanayesha <rizwanayesha2812@gmail.com>
2026-02-11 22:21:05 +0500

    Updated layout, responsive fixes and contact page

--- script.js
+++ script.js
@@ -36,3 +36,45 @@ const parent = element.parentElement;
 parent.classList.toggle("active");
 
 }
+const ctx = document.getElementById('communicationChart');
+
+if (ctx) {
+
+new Chart(ctx, {
+type: 'doughnut',
+data: {
+labels: [
+'People with Communication Challenges',
+'General Population'
+],
+datasets: [{
+data: [20, 80],
+backgroundColor: [
+'#7FA7D6',   // primary
+'#F0B88A'    // accent
+],
+borderWidth:0,
+hoverOffset:20
+}]
+},
+options: {
+responsive:true,
+cutout:'65%',
+plugins:{
+legend:{
+position:'bottom',
+labels:{
+color:'#2c2c2c',
+font:{ size:14 }
+}
+}
+},
+animation:{
+animateRotate:true,
+duration:2200,
+easing:'easeOutBounce'
+}
+}
+});
+
+}
